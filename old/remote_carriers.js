614ccfbacb4df754d0e309926422b00339315e26
CarlGibretzky <moped123@web.de>
2016-08-10 20:38:18 +0200

    carrier log bug fix

--- old/remote_carriers.js
+++ old/remote_carriers.js
@@ -54,8 +54,8 @@ function control_carrier(creep) {
                 creep.memory.returning = creep.ticksToLive < 300;
 
                 creep.memory.tours = creep.memory.tours + 1 || 1;
-                creep.memory.deposit = creep.memory.deposit + creep.memory.energy || creep.memory.energy;
-                console.log(creep.name + "[" + (creep.body.length*50) + "]" + " (tour " + creep.memory.tours + ") deposited in total " + creep.memory.deposit + ", TTL: " + creep.ticksToLive);
+                creep.memory.deposit = creep.memory.deposit + creep.carry.energy || creep.carry.energy;
+                console.log(creep.name + " [" + (creep.body.length*50) + "]" + " (tour " + creep.memory.tours + ") deposited in total " + creep.memory.deposit + ", TTL: " + creep.ticksToLive);
             }
         }
     } else {
