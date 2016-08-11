07129aed1b98bd817afd0d836321cdc15b6d0f66
CarlGibretzky <moped123@web.de>
2016-08-11 07:32:24 +0200

    HOPE!!

--- old/energy_distribution.js
+++ old/energy_distribution.js
@@ -52,7 +52,12 @@ function setup () {
     }
 }
 
-const JOB_TYPE = {
+const FLOW = {
+    IN: 'IN',
+    OUT: 'OUT'
+};
+
+const METHOD = {
     WITHDRAW: 'WITHDRAW',
     TRANSFER: 'TRANSFER',
     PICKUP: 'PICKUP',
@@ -91,9 +96,8 @@ function job_idx(jobs, id) {
 
 function jobs(room) {
     let ret = {};
-    ret[JOB_TYPE.TRANSFER] = [];
-    ret[JOB_TYPE.WITHDRAW] = [];
-    ret[JOB_TYPE.SUPPLY] = [];
+    ret[FLOW.IN] = [];
+    ret[FLOW.OUT] = [];
 
     let structures = room.find(FIND_STRUCTURES, {filter: (s) => structure_filter(s) && !s.isIgnore()});
     for (let i = 0; i < structures.length; ++i) {
@@ -101,22 +105,20 @@ function jobs(room) {
         let levels = s.getLevels();
         let fill = _.sum(s.store);
         if (fill < levels.min) {
-            ret[JOB_TYPE.TRANSFER].push({amount: levels.max - s.store.energy, id: s.id});
+            ret[FLOW.OUT].push({amount: levels.max - s.store.energy, method: METHOD.TRANSFER, id: s.id});
         } else if (fill > levels.max) {
-            ret[JOB_TYPE.WITHDRAW].push({amount: s.store.energy - levels.max,  id: s.id});
+            ret[FLOW.IN].push({amount: s.store.energy - levels.max, method: METHOD.WITHDRAW, id: s.id});
         }
     }
-/*
     let supplyables = room.find(FIND_MY_STRUCTURES, {filter: (s) => supplyable_filter(s)});
     for (let i = 0; i < supplyables.length; ++i) {
         let s = supplyables[i];
         let fill = s.energy;
         let cap = s.energyCapacity;
         if (fill < cap) {
-            ret[JOB_TYPE.SUPPLY].push({amount: cap - fill, id: s.id});
+            ret[FLOW.OUT].push({amount: cap - fill, id: s.id});
         }
     }
-   */
     return ret;
 }
 
@@ -124,64 +126,72 @@ function selectJob(creep, available_jobs) {
     creep.memory.job = null;
 
     if (creep.carry.energy < 50 && creep.ticksToLive >= 35) {
-        if (available_jobs[JOB_TYPE.WITHDRAW].length > 0) {
+        if (available_jobs[FLOW.IN].length > 0) {
             let structures = [];
-            let jobs = available_jobs[JOB_TYPE.WITHDRAW];
+            let jobs = available_jobs[FLOW.IN];
             jobs.forEach((e) => structures.push(Game.getObjectById(e.id)));
             let target = creep.pos.findClosestByPath(structures);
             if (target) {
                 let idx = job_idx(jobs, target.id);
                 if (idx >= 0) {
                     let job = jobs[idx];
-                    creep.memory.job = {type: JOB_TYPE.WITHDRAW, id: job.id};
+                    creep.memory.job = {type: FLOW.IN, method: job.method, id: job.id};
                     job.amount = Math.max(0, job.amount - creep.carryCapacity + _.sum(creep.carry));
                     if (job.amount == 0) jobs.splice(idx, 1);
                 }
             }
-        } else if (available_jobs[JOB_TYPE.TRANSFER].length > 0) {
+        } else if (available_jobs[FLOW.OUT].length > 0) {
             if (creep.room.storage && creep.room.storage.getLevels().min <= creep.room.storage.store.energy - 50) {
-                creep.memory.job = {type: JOB_TYPE.WITHDRAW, id: creep.room.storage.id};
+                creep.memory.job = {type: FLOW.IN, method: METHOD.WITHDRAW, id: creep.room.storage.id};
             }
         }
-    } else if (available_jobs[JOB_TYPE.TRANSFER].length > 0) {
+    } else if (available_jobs[FLOW.OUT].length > 0) {
         let structures = [];
-        let jobs = available_jobs[JOB_TYPE.TRANSFER]
+        let jobs = available_jobs[FLOW.OUT]
         jobs.forEach((e) => structures.push(Game.getObjectById(e.id)));
         let target = creep.pos.findClosestByPath(structures);
         if (target) {
             let idx = job_idx(jobs, target.id);
             if (idx >= 0) {
                 let job = jobs[idx];
-                creep.memory.job = {type: JOB_TYPE.TRANSFER, id: job.id};
+                creep.memory.job = {type: FLOW.OUT, method: job.method, id: job.id};
                 job.amount = Math.max(0, job.amount - creep.carry.energy);
                 if (job.amount == 0) jobs.splice(idx, 1);
             }
         }
     } else {
         if (creep.room.storage && creep.room.storage.getLevels().max >= creep.room.storage.store.energy + creep.carry.energy) {
-            creep.memory.job = {type: JOB_TYPE.TRANSFER, id: creep.room.storage.id};
+            creep.memory.job = {type: FLOW.OUT, method: METHOD.TRANSFER, id: creep.room.storage.id};
         } else if (creep.ticksToLive < 35) {
             var struct = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => structure_filter(s) && _.sum(s.store) + creep.carry.energy / 4 < s.storeCapacity});
             if (struct) {
-                creep.memory.job = {type: JOB_TYPE.TRANSFER, id: struct.id};
+                creep.memory.job = {type: FLOW.OUT, method: METHOD.TRANSFER, id: struct.id};
             }
         }
     }
 }
 
 function isValid(creep, job) {
-    let struct = Game.getObjectById(job.id);
-    if (!struct) return false;
-    if (struct.isIgnore()) return false;
-    let levels = struct.getLevels();
-    if (!levels) return false;
+    let obj = Game.getObjectById(job.id);
+    if (!obj) return false;
+    if (job.method == METHOD.TRANSFER || job.method == METHOD.WITHDRAW) {
+        if (obj.isIgnore()) return false;
+        var levels = struct.getLevels();
+        if (!levels) return false;
+    }
 
-    if (job.type == JOB_TYPE.WITHDRAW) {
+    if (job.method == METHOD.WITHDRAW) {
         if (_.sum(creep.carry) == creep.carryCapacity) return false;
-        if (struct.store.energy <= levels.min) return false;
-    } else if (job.type == JOB_TYPE.TRANSFER) {
+        if (obj.store.energy <= levels.min) return false;
+    } else if (job.method == METHOD.TRANSFER) {
         if (_.sum(creep.carry) < 50) return false;
-        if (struct.store.energy >= levels.max) return false;
+        if (obj.store.energy >= levels.max) return false;
+    } else if (job.method == METHOD.SUPPLY) {
+        if (_.sum(creep.carry) < Math.min(50, obj.energyCapacity - obj.energy)) return false;
+        if (obj.energy == obj.energyCapacity) return false;
+    } else {
+        console.log("ERR: UNKNOWN job.method in energy_distribution.isValid");
+        return false;
     }
 
     return true;
@@ -194,13 +204,20 @@ function control_carrier(creep, room, available_jobs) {
             if (target) {
                 if (isValid(creep, creep.memory.job)) {
                     if (creep.pos.inRangeTo(target, 1)) {
-                        if (creep.memory.job.type == JOB_TYPE.WITHDRAW) {
+                        if (creep.memory.job.type == FLOW.IN) {
                             let amount = Math.min(target.store.energy - target.getLevels().min, creep.carryCapacity - _.sum(creep.carry));
                             if (creep.withdraw(target, RESOURCE_ENERGY, amount) == OK) {
                                 creep.memory.job = null;
                             }
-                        } else if (creep.memory.job.type == JOB_TYPE.TRANSFER) {
-                            let amount = Math.min(target.getLevels().max - target.store.energy, creep.carry.energy);
+                        } else if (creep.memory.job.type == FLOW.OUT) {
+                            let amount = 0;
+                            if (creep.memory.job.method == METHOD.TRANSFER) {
+                                amount = Math.min(target.getLevels().max - target.store.energy, creep.carry.energy);
+                            } else if (creep.memory.job.method == METHOD.SUPPLY) {
+                                amount = Math.min(target.energyCapacity - target.energy, creep.carry.energy);
+                            } else {
+                                console.log("ERR: INVALID job.type / job.method combination in energy_distribution.control_carrier");
+                            }
                             if (creep.transfer(target, RESOURCE_ENERGY, amount) == OK) {
                                 creep.memory.job = null;
                             }
