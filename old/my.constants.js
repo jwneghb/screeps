310234e613adfb45db38fed8a48437ccf2439521
CarlGibretzky <moped123@web.de>
2016-08-11 11:00:22 +0200

    edited perimeter

--- old/my.constants.js
+++ old/my.constants.js
@@ -1,9 +1,8 @@
 module.exports = {
     perimeter: {
         W42N24: function (x, y) {
-            if (x <= 19 && y <= 42) return true;
-            if (x <= 28 && y <= 34) return true;
-            if (x <= 45 && y <= 24) return true;
+            if (x <= 19 && y <= 43) return true;
+            if (x <= 45 && y <= 40) return true;
             if (y <= 19) return true;
             return false;
         }
