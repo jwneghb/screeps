98433748cf1a7c86a651a5f1f8d4a2d462616230
CarlGibretzky <moped123@web.de>
2016-08-11 15:01:09 +0200

    Link test.

--- old/main.js
+++ old/main.js
@@ -121,5 +121,30 @@ module.exports.loop = function () {
             }
         }
     }
+
+    // LINK CONTROL
+    var src_link0 = Game.getObjectById('57ac3824aa842ca30fca0816');
+    var src_link1 = Game.getObjectById('57ac6a7a7212280974c3c793');
+    var dst_link = Game.getObjectById('57aac04f4a57a8840300a445');
+    var src = null;
+    if (dst_link.energy <= dst_link.energyCapacity - 97) {
+        if (src_link0.cooldown > 0) {
+            if (src_link1.cooldown == 0) src = src_link1;
+        } else if (src_link1.cooldown > 0) {
+            src = src_link0;
+        }
+        if (!src) {
+            if (src_link0.energy > src_link1.energy) {
+                if (src_link0.energy >= 100) src = src_link0;
+            } else if (src_link1.energy >= 100) {
+                src = src_link1;
+            }
+        }
+        if (src) {
+            var n = Math.min(Math.floor((dst_link.energyCapacity - dst_link.energy)/97), Math.floor(src.energy/100));
+            src.transferEnergy(dst_link, n * 100);
+        }
+    }
+
 };
 
