4d9a355e14f4e2adcada2ed2b153ce7c59c0e51b
CarlGibretzky <moped123@web.de>
2016-08-12 07:08:15 +0200

    WEEKEND SETTINGS

--- old/main.js
+++ old/main.js
@@ -54,38 +54,57 @@ module.exports.loop = function () {
         }
     }
 
-    var minerW41N24_ttl = new_mining.control('W41N24');
-    if (minerW41N24_ttl.length > 0) {
-        if (minerW41N24_ttl[0] < 180) {
-            var creep = Game.spawns.spawn_01.createCustomCreep(creepTypes.FAST_MINER, Infinity);
-            if (! (creep < 0)) {
-                new_mining.assign(Game.creeps[creep], 'W41N24');
-            }
-        }
-    }
-
-    var carrier_status = carriers.control();
-    if ((!carrier_status.W41N24 || carrier_status.W41N24.creeps.length < 3) && new_mining.fill('W41N24') > 500) {
-        var creep = Game.spawns.spawn_01.createCustomCreep(creepTypes.FAST_TRANSPORTER);
-        if (! (creep < 0)) {
-            carriers.assign(creep, 'W41N24');
-        }
-    }
-
-    goto.run();
-
-    var work = colony.run(Game.rooms.W42N25);
-    if (work < 4 && Game.rooms.W42N25.energyAvailable > 1000) Game.spawns.spawn_02.createCustomCreep('MEDIUM_WORKER');
-
-    if (edist.control(Game.rooms.W42N25) < 3) {
-        var creep = Game.spawns.spawn_02.createCustomCreep('MEDIUM_TRANSPORT');
-        if (! (creep < 0)) edist.assign(creep, 'W42N25');
-    }
-
     if (edist.control(Game.rooms.W42N24) < 2) {
         var creep = Game.spawns.spawn_01.createCustomCreep('MEDIUM_TRANSPORT', 1200);
         if (! (creep < 0)) edist.assign(creep, 'W42N24');
     }
+
+    // --- W41N24 ---
+
+    var minerW41N24_ttl = new_mining.control('W41N24');
+    var carrier_status = carriers.control();
+
+    // WEEKEND: Stop LDM, if energy drops below 450k
+    if (Game.rooms.W42N24.storage.store.energy > 450000) {
+
+        if (minerW41N24_ttl.length > 0) {
+            if (minerW41N24_ttl[0] < 180) {
+                var creep = Game.spawns.spawn_01.createCustomCreep(creepTypes.FAST_MINER, Infinity);
+                if (!(creep < 0)) {
+                    new_mining.assign(Game.creeps[creep], 'W41N24');
+                }
+            }
+        }
+
+        if ((!carrier_status.W41N24 || carrier_status.W41N24.creeps.length < 3) && new_mining.fill('W41N24') > 500) {
+            var creep = Game.spawns.spawn_01.createCustomCreep(creepTypes.FAST_TRANSPORTER);
+            if (!(creep < 0)) {
+                carriers.assign(creep, 'W41N24');
+            }
+        }
+
+    }
+
+    // --- /W41N24 ---
+
+    goto.run();
+
+    // --- W42N25
+
+    // BUILD NEW TOWER
+    if (Game.rooms.W42N25.controller.level == 5) {
+        Game.rooms.W42N25.createConstructionSite(27, 45, STRUCTURE_TOWER);
+    }
+
+    var work = colony.run(Game.rooms.W42N25);
+    if (work < 3 && Game.rooms.W42N25.energyAvailable > 1000) Game.spawns.spawn_02.createCustomCreep('MEDIUM_WORKER'); // WEEKEND: 1 less
+
+    if (edist.control(Game.rooms.W42N25) < 2) { // WEEKEND: 1 less
+        var creep = Game.spawns.spawn_02.createCustomCreep('MEDIUM_TRANSPORT');
+        if (! (creep < 0)) edist.assign(creep, 'W42N25');
+    }
+
+    // --- /W42N25 ---
 
     var reserved_rooms = ['W41N24', 'W41N25'];
     var reserver_spawns = {W41N24: {spawn: Game.spawns.spawn_01, busy: false}, W41N25: {spawn: Game.spawns.spawn_02, busy: false}};
