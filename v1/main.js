6e5bf2a78cc21b99466dca955606a8db0bc812e1
CarlGibretzky <moped123@web.de>
2016-08-15 11:30:01 +0200

    err

--- v1/main.js
+++ v1/main.js
@@ -45,7 +45,7 @@ module.exports.loop = function() {
     var distributors01 = distributors.control(Game.rooms.W42N24);
     if (distributors01 < 2) spawn_at01(dist_body, (name) => distributors.assign(name, 'W42N24'));
 
-    var miners01 = mining.control(Game.rooms.W42N24);
+    var miners01 = mining.control('W42N24');
     if (miners01.length < 2 || miners01[0] < 40) spawn_at01(miner_body, (name) => mining.assign(name, 'W42N24'));
 
     workers.run(Game.rooms.W42N24, spawn_at01);
@@ -54,7 +54,7 @@ module.exports.loop = function() {
     var distributors02 = distributors.control(Game.rooms.W42N25);
     if (distributors02 < 2) spawn_at02(dist_body, (name) => distributors.assign(name, 'W42N25'));
 
-    var miners02 = mining.control(Game.rooms.W42N25);
+    var miners02 = mining.control('W42N25');
     if (miners02.length < 2 || miners02[0] < 40) spawn_at02(miner_body, (name) => mining.assign(name, 'W42N25'));
 
     var workers02 = colony.run(Game.rooms.W42N25);
