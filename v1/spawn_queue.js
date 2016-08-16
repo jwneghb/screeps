4fda6c67a101486b6cc1de43af6aa1fac5f074f8
CarlGibretzky <moped123@web.de>
2016-08-16 15:54:22 +0200

    changed & fixes

--- v1/spawn_queue.js
+++ v1/spawn_queue.js
@@ -1,5 +1,5 @@
 module.exports = {
-    tick: tick,
+    clock: tock,
     spawn: schedule_spawn,
     status: ticket_status
 };
@@ -12,40 +12,45 @@ function body_attr (body) {
     return {cost: cost, duration: 3 * body.length};
 }
 
-function tick (room) {
-    if (!Memory.SQ_v1) Memory.SQ_v1 = {rooms: {}, open: {}};
-    if (!Memory.SQ_v1.rooms[room.name]) Memory.SQ_v1.rooms[room.name] = {queue: []};
+function tock () {
+    if (!Memory.SQ_v1) Memory.SQ_v1 = {queues: {}, open: {}};
     Memory.SQ_v1.ticket = 0;
+    for (var i in Memory.SQ_v1.queues) {
+        if (Game.rooms[i]) {
+            tick(Game.rooms[i]);
+        }
+    }
+}
 
+function tick (room) {
     for (var i in Game.creeps) {
         delete Memory.SQ_v1.open[i];
     }
 
-    var room_data = Memory.SQ_v1.rooms[room.name];
+    var queue = Memory.SQ_v1.queues[room.name];
 
     var idx = -1;
-    for (var i = 0; i < room_data.queue.length; ++i) {
-        room_data.queue[i].priority.total += room_data.queue[i].priority.inc;
-        if (idx < 0 || room_data.queue[idx].priority.total < room_data.queue[i].priority.total) {
-            if (room_data.queue[idx].priority.total >= room_data.queue[idx].priority.min) {
+    for (var i = 0; i < queue.length; ++i) {
+        queue[i].priority.total += queue[i].priority.inc;
+        if (idx < 0 || queue[idx].priority.total < queue[i].priority.total) {
+            if (queue[idx].priority.total >= queue[idx].priority.min) {
                 idx = i;
             }
         }
     }
 
-    if (idx >= 0 && room_data.open[room_data.queue[idx].ticket].attr.cost <= room.energyAvailable) {
+    if (idx >= 0 && queue[idx].attr.cost <= room.energyAvailable) {
 
         var spawns = room.find(FIND_MY_SPAWNS);
         for (var i = 0; i < spawns.length; ++i) {
             var spawn = spawns[i];
             if (!spawn.spawning) {
-                var qi = room_data.queue[idx];
+                var qi = queue[idx];
                 var name = spawn.createCreep(qi.body, qi.ticket);
                 if (!(name < 0)) {
                     if (qi.assignment) qi.assignment(name);
-                    room_data.spawns[spawn.name].busy = Game.time + qi.attr.duration;
                     Memory.SQ_v1.open[qi.ticket].spawning = true;
-                    room_data.queue.splice(idx, 1);
+                    queue.splice(idx, 1);
                     return true;
                 }
             }
@@ -61,13 +66,15 @@ function ticket_status(ticket) {
 }
 
 function schedule_spawn (room, body, assignment=null, inc=1, init=0, min=0) {
+    if (!Memory.SQ_v1.queues[room.name]) Memory.SQ_v1.queues[room.name] = [];
+
     var attr = body_attr(body);
     if (attr.cost > room.energyCapacityAvailable) return ERR_NOT_ENOUGH_ENERGY;
 
     Memory.SQ_v1.ticket += 1;
     var ticket = Game.time.toString(16) + 'x' + Memory.SQ_v1.ticket.toString(16);
 
-    Memory.SQ_v1.rooms[room.name].queue.push({ticket: ticket, attr: attr, body: body, priority: {inc: inc, total: init, min: min}, assignment: assignment});
+    Memory.SQ_v1.queues[room.name].push({ticket: ticket, attr: attr, body: body, priority: {inc: inc, total: init, min: min}, assignment: assignment});
     Memory.SQ_v1.open[ticket] = {spawning: false};
 
     return ticket;
